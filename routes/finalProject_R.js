8e81b5033452f8d410dbe42634d0ef1f041273fc
dududavid <gazit.david@gmail.com>
2025-09-16 23:53:29 +0300

    fs + multer

--- routes/finalProject_R.js
+++ routes/finalProject_R.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 
-let products = [];
+let project = [];
 let nextID = 1;
 
+if(!fs.existsSync('images')){
+    fs.mkdirSync('images');
+}
+const storage =multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'images/');
+    },
+    filename: (req,file,cb)=>{
+        let id =req.params.id ? req.params.id : nextId;
+        let finalFileName= `${id}${path.extname(file.originalname)}`;
+        cb(null,finalFileName);
+    }
+});
+
 router.get('/', (req, res) => {
-    res.json(products);
+    res.json(project);
+})
+
+router.post('/', (req, res) => {
+    let id = req.body.id;
+    let name = req.body.name;
+    let project = {id,name};
+    project[id] = project;
+    res.json({message:"ok"});
 })
 
 module.exports = router;
