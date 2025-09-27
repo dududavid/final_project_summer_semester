05e1e1eb019378bd1a4a001a7b47d75fac6f3278
dududavid <gazit.david@gmail.com>
2025-09-27 20:54:08 +0300

    added get data

--- public/script.js
+++ public/script.js
@@ -2,4 +2,15 @@ function addTitle(){
     let txt = "פרוייקט סיום";
     document.getElementById("h1").innerText = txt;
 }
+
+async function getData() {
+    try {
+        let response = await fetch("/p");
+        let data = await response.json();
+        createGrid(data);
+    } catch (err) {
+        alert(err);
+    }
+}
+
 addTitle();
