752b6baafe55533e9520c659f18455b1160f6436
dududavid <gazit.david@gmail.com>
2025-09-27 20:59:14 +0300

    added creat grid

--- public/script.js
+++ public/script.js
@@ -13,4 +13,29 @@ async function getData() {
     }
 }
 
+function createGrid(data) {
+    let voted = JSON.parse(localStorage.getItem("votedProjects")) || [];
+    voted = voted.map(Number);
+    let txt = "";
+    for (let obj of data) {
+        if (obj) {
+            let disabled = voted.includes(obj.id) ? "disabled" : "";
+            txt += `
+      <div class="card">
+        <div onclick="openProject(${obj.id})" style="cursor:pointer">
+           <img src="/images/${obj.myFileName}?t=${Date.now()}" alt="${obj.name}">
+           <p>${obj.name}</p>
+           <div>${obj.description}</div>
+        </div>
+        <div>
+             <button onClick="deleteProject(${obj.id})">Delete</button>
+             <button onClick="getById(${obj.id})">Edit</button>
+             <button onClick="vote(${obj.id})" ${disabled}>👍 הצבע (${obj.votes || 0})</button>
+        </div>
+      </div>`;
+        }
+    }
+    document.getElementById("main").innerHTML = txt;
+}
+
 addTitle();
